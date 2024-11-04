53d253994ad4e6c8f07f6898dc09a292ca5e8d51
bhav380 <bhaweshrathour18@gmail.com>
2024-11-04 12:59:40 +0530

    changes in predict

--- app.py
+++ app.py
@@ -27,12 +27,13 @@ def predict():
     
     print("HIIIIII")
     print(model.summary())
-    # data = request.get_json(force=True)
+    data = request.get_json(force=True)
     # print(data)
-    # image = np.vstack(data).reshape(64,64)
-    # image = image.reshape(1,64,64,1)
-    # y = model.predict(image,verbose=1)
-    # top_3 =  np.argsort(-y)[:, 0:6]
+    image = np.vstack(data).reshape(64,64)
+    image = image.reshape(1,64,64,1)
+    y = model.predict(image,verbose=1)
+    print(y)
+    top_3 =  np.argsort(-y)[:, 0:6]
     # pred_results = []
     # pred_results.append(top_3)
     # pred_results = np.concatenate(pred_results)
@@ -44,10 +45,10 @@ def predict():
     # result.append(preds_df['first'].iloc[0])
     # result.append(preds_df['second'].iloc[0])
     # result.append(preds_df['third'].iloc[0])
-    # # print(result)
+    print(top_3)
     # print(jsonify(result))
     print("done00")
-    return "III"
+    return(jsonify(top_3))
 
 if __name__ == '__main__':
     app.run(debug=True)
