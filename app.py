04ebe5988668ece5180f8a17a48b3c329bc829d1
bhav380 <bhaweshrathour18@gmail.com>
2024-11-04 17:36:00 +0530

    changed predict controller

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-from flask import Flask, request, jsonify
-import json
-import pandas as pd
+from flask import Flask, request
 import numpy as np
 from tensorflow import keras
 from flask_cors import CORS
@@ -8,7 +6,14 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+# file_path = './categories.json' 
 
+# with open(file_path, 'r') as file:
+#     data = json.load(file) 
+#     classes_path = data['categories']
+# classes_path = sorted(classes_path, key=lambda s: s.lower())
+# class_dict = {x[:-4].replace(" ", "_"):i for i, x in enumerate(classes_path)}
+# path  = "../dataset/train_simplified/train_simplified"
 
 
 
@@ -28,12 +33,16 @@ def predict():
     print("HIIIIII")
     print(model.summary())
     data = request.get_json(force=True)
+    data = np.array(data)
     # print(data)
-    image = np.vstack(data).reshape(64,64)
-    image = image.reshape(1,64,64,1)
-    y = model.predict(image,verbose=1)
+    # image = np.vstack(data).reshape(64,64)
+    # image = image.reshape(1,64,64,1)
+    y = model.predict(data,verbose=1)
     print(y)
-    top_3 =  np.argsort(-y)[:, 0:6]
+    top_3 =  np.argsort(-y)[:, 0:3]
+    print("______________________________________________top3")
+    print(top_3)
+    print(top_3[0])
     # pred_results = []
     # pred_results.append(top_3)
     # pred_results = np.concatenate(pred_results)
@@ -45,10 +54,9 @@ def predict():
     # result.append(preds_df['first'].iloc[0])
     # result.append(preds_df['second'].iloc[0])
     # result.append(preds_df['third'].iloc[0])
-    print(top_3)
-    # print(jsonify(result))
+    # print()
     print("done00")
-    return(jsonify(top_3))
+    return(top_3[0].tolist())
 
 if __name__ == '__main__':
     app.run(debug=True)
