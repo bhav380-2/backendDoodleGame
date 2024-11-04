3f1817cd30b12863eb6defc4f172b040aeffab32
bhav380 <bhaweshrathour18@gmail.com>
2024-11-04 12:46:07 +0530

    commit

--- app.py
+++ app.py
@@ -8,14 +8,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-file_path = './categories.json' 
 
-with open(file_path, 'r') as file:
-    data = json.load(file) 
-    classes_path = data['categories']
-classes_path = sorted(classes_path, key=lambda s: s.lower())
-class_dict = {x[:-4].replace(" ", "_"):i for i, x in enumerate(classes_path)}
-path  = "../dataset/train_simplified/train_simplified"
 
 
 
@@ -31,27 +24,30 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    
     print("HIIIIII")
-    data = request.get_json(force=True)
-    print(data)
-    image = np.vstack(data).reshape(64,64)
-    image = image.reshape(1,64,64,1)
-    y = model.predict(image,verbose=1)
-    top_3 =  np.argsort(-y)[:, 0:6]
-    pred_results = []
-    pred_results.append(top_3)
-    pred_results = np.concatenate(pred_results)
-    reverse_dict = {v: k for k, v in class_dict.items()}
-    preds_df = pd.DataFrame({'first': pred_results[:,0], 'second': pred_results[:,1], 'third': pred_results[:,2]})
-    preds_df = preds_df.replace(reverse_dict)
+    print(model.summary())
+    # data = request.get_json(force=True)
+    # print(data)
+    # image = np.vstack(data).reshape(64,64)
+    # image = image.reshape(1,64,64,1)
+    # y = model.predict(image,verbose=1)
+    # top_3 =  np.argsort(-y)[:, 0:6]
+    # pred_results = []
+    # pred_results.append(top_3)
+    # pred_results = np.concatenate(pred_results)
+    # reverse_dict = {v: k for k, v in class_dict.items()}
+    # preds_df = pd.DataFrame({'first': pred_results[:,0], 'second': pred_results[:,1], 'third': pred_results[:,2]})
+    # preds_df = preds_df.replace(reverse_dict)
 
-    result = []
-    result.append(preds_df['first'].iloc[0])
-    result.append(preds_df['second'].iloc[0])
-    result.append(preds_df['third'].iloc[0])
-    # print(result)
-    print(jsonify(result))
-    return jsonify(result)
+    # result = []
+    # result.append(preds_df['first'].iloc[0])
+    # result.append(preds_df['second'].iloc[0])
+    # result.append(preds_df['third'].iloc[0])
+    # # print(result)
+    # print(jsonify(result))
+    print("done00")
+    return "III"
 
 if __name__ == '__main__':
     app.run(debug=True)
